307a00baeb2865d2d1e623220e189c21ceee77a1
Mugenzijmv <mdesignpro10@gmail.com>
2026-08-16 14:48:02 +0200

    changes on sidebar

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -62,7 +62,7 @@ export default async function Layout({ children }: Readonly<{ children: ReactNod
             <div className="flex items-center gap-2">
               <LayoutControls />
               <ThemeSwitcher />
-              <GitHubRepositoriesMenu />
+              {/* <GitHubRepositoriesMenu /> */}
               <AccountSwitcher users={users} />
             </div>
           </div>
